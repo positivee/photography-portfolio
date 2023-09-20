79dfe1bc59412456036cdd58e10103de610120c9
positivee <biero.kamil@gmail.com>
2023-09-20 12:13:20 +0200

    commented off galerry

--- app/(layout)/page.tsx
+++ app/(layout)/page.tsx
@@ -54,7 +54,7 @@ export default async function Home() {
               </p>
             </div>
           </div>
-          {/* <Gallery images={images} slug={"photo"} /> */}
+          <Gallery images={images} slug={"photo"} />
           <PackageOfferSection />
         </div>
       </main>
